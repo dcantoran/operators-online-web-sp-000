8d47e6bae66f0584bb11181b24b9c229f75f9033
David Cantoran <davidcantoran36@gmail.com>
2019-11-22 20:41:21 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,6 @@
 def unsafe?(speed)
-  speed > 60 ? true : false 
-  speed < 40 ? true : false 
+  # speed > 60 ? true : false 
+  # speed < 40 ? true : false 
   speed < 60 && speed > 40 ? false : true 
 	
 end
@@ -8,6 +8,7 @@ end
 
 
 def not_safe?(speed)
+  # speed > 60 
 end
 	
 
